4909864f3bdb9ead235e67ba3e6e654990763e4d
1234tgk <1234tgk@gmail.com>
2020-05-22 12:19:26 -0400

    Initial UI for testing

---
+++ app/src/test/java/com/example/livelydarkness/TestingForCalculateTime.java
@@ -0,0 +1,16 @@
+package com.example.livelydarkness;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestingForCalculateTime {
+
+    @Test
+    public void testCalculateTime() {
+        CalculateTime test = new CalculateTime("ENTER 0100 EXIT 0200 ENTER 0300 EXIT 0400 ENTER 1000");
+        long[] result = test.calculateTimeDiff();
+
+        Assert.assertEquals(200, result[0]);
+        Assert.assertEquals(700, result[1]);
+    }
+}
