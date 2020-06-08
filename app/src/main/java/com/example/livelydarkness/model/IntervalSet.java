d06b4d87f8b1d9a3d5a1685856783028ab1f3d02
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-08 20:43:08 +0900

    Fix Formatting

--- app/src/main/java/com/example/livelydarkness/model/IntervalSet.java
+++ app/src/main/java/com/example/livelydarkness/model/IntervalSet.java
@@ -5,18 +5,17 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IntervalSet<T extends Comparable<T>> {
     private ArrayList<Interval<T>> intervals;
+
     @SafeVarargs
-    public IntervalSet(Interval<T> ...intervals) {
+    public IntervalSet(Interval<T>... intervals) {
         buildIntervalSet(Arrays.stream(intervals));
     }
 
